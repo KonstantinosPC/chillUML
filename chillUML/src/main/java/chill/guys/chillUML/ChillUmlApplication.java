399f8d74adcc8e680720fcb1f89b10732a43c4ad
KonstantinosPC <80127970+KonstantinosPC@users.noreply.github.com>
2026-03-19 14:10:48 +0200

    Add Spring Files

---
+++ chillUML/src/main/java/chill/guys/chillUML/ChillUmlApplication.java
@@ -0,0 +1,13 @@
+package chill.guys.chillUML;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChillUmlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChillUmlApplication.class, args);
+	}
+
+}
