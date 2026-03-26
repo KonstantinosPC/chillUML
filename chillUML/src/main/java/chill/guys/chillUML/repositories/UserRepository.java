16909e6e4b7c6fb797ecf869cc3ae989550f026a
cs05457 <anagnosthsk@gmail.com>
2026-03-26 13:15:49 +0200

    User,UserRepository,UserServices,UserServicesImpl

---
+++ chillUML/src/main/java/chill/guys/chillUML/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package chill.guys.chillUML.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import chill.guys.chillUML.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>{
+    User findById(int id);
+    User findByUsername(String username);
+    User deleteById(int id);
+    User findByEmail(String email);
+}
