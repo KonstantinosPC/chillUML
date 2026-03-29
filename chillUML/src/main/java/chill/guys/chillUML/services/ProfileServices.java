0b2e7212faa9ee24915f994f1e539758798d2300
ANASTASIOSBELLOS <cs05474@uoi.gr>
2026-03-29 15:08:19 +0300

    All changes

--- chillUML/src/main/java/chill/guys/chillUML/services/ProfileServices.java
+++ chillUML/src/main/java/chill/guys/chillUML/services/ProfileServices.java
@@ -1,9 +1,8 @@
 package chill.guys.chillUML.services;
 
 public interface ProfileServices {
-    public void changeProfilePicture(String profilePicture);
-    public void changeProfileUsername(String profilePicture);
-    public void changeProfilePassword(String profilePicture);
-    public void changeProfileEmail(String profilePicture);
-    // (OPTIONAL) public void deleteAccount()
+    void changeProfilePicture(String profilePicture);
+    void changeProfileUsername(String username);
+    void changeProfileEmail(String email);
+    void changeProfilePassword(String currentPassword, String newPassword);
 }
--- chillUML/src/main/java/chill/guys/chillUML/services/ProfileServicesImpl.java
+++ chillUML/src/main/java/chill/guys/chillUML/services/ProfileServicesImpl.java
@@ -1,42 +1,64 @@
 package chill.guys.chillUML.services;
 
+import chill.guys.chillUML.domain.User;
 import chill.guys.chillUML.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProfileServicesImpl implements ProfileServices{
-    CharSequence specialChars = "+-*/%=!<>&|^~(){}[];,.?:@_$";
-    @Autowired
-    private final PasswordEncoder passwordEncoder = new MessageDigestPasswordEncoder("SHA-256");
+public class ProfileServicesImpl implements ProfileServices {
 
     @Autowired
     private UserRepository userDAO;
 
+    private final PasswordEncoder passwordEncoder = new MessageDigestPasswordEncoder("SHA-256");
+
+    // Βοηθητική μέθοδος για να βρίσκουμε τον τρέχοντα συνδεδεμένο χρήστη
+    private User getAuthenticatedUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return userDAO.findByUsername(username);
+    }
 
     @Override
     public void changeProfilePicture(String profilePicture) {
-
+        User user = getAuthenticatedUser();
+        if (user != null) {
+            user.setProfilePicture(profilePicture);
+            userDAO.save(user);
+        }
     }
 
     @Override
-    public void changeProfileUsername(String profilePicture) {
-
+    public void changeProfileUsername(String username) {
+        User user = getAuthenticatedUser();
+        if (user != null) {
+            user.setUsername(username);
+            userDAO.save(user);
+        }
     }
 
     @Override
-    public void changeProfilePassword(String profilePicture) {
-
+    public void changeProfileEmail(String email) {
+        User user = getAuthenticatedUser();
+        if (user != null) {
+            user.setEmail(email);
+            userDAO.save(user);
+        }
     }
 
     @Override
-    public void changeProfileEmail(String profilePicture) {
-
-    }
-
-    private verifyPassword(String password){
-
+    public void changeProfilePassword(String currentPassword, String newPassword) {
+        User user = getAuthenticatedUser();
+        if (user != null) {
+            String hashedCurrent = passwordEncoder.encode(currentPassword);
+            if (user.verifyPassword(hashedCurrent)) {
+                String hashedNew = passwordEncoder.encode(newPassword);
+                user.setPassword(hashedNew);
+                userDAO.save(user);
+            }
+        }
     }
 }
