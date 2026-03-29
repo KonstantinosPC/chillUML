34555be5455166d4153e75640a4b3756b5c367f1
ANASTASIOSBELLOS <cs05474@uoi.gr>
2026-03-29 11:24:06 +0300

    UserRepository && UserService

---
+++ chillUML/src/main/java/chill/guys/chillUML/domain/User.java
@@ -0,0 +1,11 @@
+package chill.guys.chillUML.domain;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = User)
+public class User{
+
+    @Id
+
+}
---
+++ chillUML/src/main/java/chill/guys/chillUML/services/UserService.java
@@ -0,0 +1,10 @@
+package chill.guys.chillUML.services;
+import chill.guys.chillUML.domain.User;
+
+public interface UserService {
+    public void saveUser(User user);
+    public boolean isUserPresent(User user);
+    public boolean login(String username,String password);
+    public User findById(int id);
+    public User findByEmail(String email);
+}
