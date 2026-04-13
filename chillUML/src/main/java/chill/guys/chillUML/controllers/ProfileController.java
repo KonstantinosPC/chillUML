488fb2ae2e7190912e77cc7d1efbbcfdd6085f2a
KonstantinosPC <80127970+KonstantinosPC@users.noreply.github.com>
2026-04-13 11:05:09 +0300

    Template Changes

--- chillUML/src/main/java/chill/guys/chillUML/controllers/ProfileController.java
+++ chillUML/src/main/java/chill/guys/chillUML/controllers/ProfileController.java
@@ -1,5 +1,13 @@
 package chill.guys.chillUML.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class ProfileController {
 
+    @GetMapping("/profile")
+    public String toProfile(){
+        return "dashboard";
+    }
 }
