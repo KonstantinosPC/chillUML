4492889b4ae7ee56a0a6e3857816fcee3f417493
KonstantinosPC <80127970+KonstantinosPC@users.noreply.github.com>
2026-05-19 04:21:45 +0000

    remove useless files

--- chillUML/src/main/java/chill/guys/chillUML/repositories/CRCCollaboratorRepository.java
+++
@@ -1,9 +0,0 @@
-package chill.guys.chillUML.repositories;
-
-import chill.guys.chillUML.domain.CRCCollaborator;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CRCCollaboratorRepository extends JpaRepository<CRCCollaborator, Integer> {
-}
