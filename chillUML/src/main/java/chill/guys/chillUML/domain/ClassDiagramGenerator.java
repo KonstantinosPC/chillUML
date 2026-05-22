916c8b49c4db63f594bda34521528d34bb8076cc
KonstantinosPC <80127970+KonstantinosPC@users.noreply.github.com>
2026-05-22 13:34:29 +0000

    remove damaged files

--- chillUML/src/main/java/chill/guys/chillUML/domain/ClassDiagramGenerator.java
+++
@@ -1,7 +0,0 @@
-package chill.guys.chillUML.domain;
-
-import java.util.List;
-
-public interface ClassDiagramGenerator {
-    public String generateClassDiagram(List<CRC> crcs);
-}
--- chillUML/src/main/java/chill/guys/chillUML/domain/UseCaseDiagramGenerator.java
+++
@@ -1,7 +0,0 @@
-package chill.guys.chillUML.domain;
-
-import java.util.List;
-
-public interface UseCaseDiagramGenerator {
-    public String generateDiagram(List<UseCase> usecases);
-}
